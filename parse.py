26d6fa8a11de96c3ed73f7ae4e6c27384bba2f17
Irina Baldwin <2007inb@gmail.com>
2019-02-03 07:28:25 +0000

    Lint

--- parse.py
+++ parse.py
@@ -1,10 +1,10 @@
+"""Data Visualisation Project www.newcoder.io/dataviz"""
 import csv
 
 MY_FILE = "sample_sfpd_incident_all.csv"
 
 def parse(raw_file, delimiter):
     """Parses a raw CSV file to a JSON-like object"""
-    
     opened_file = open(raw_file)
 
     csv_data = csv.reader(opened_file, delimiter=delimiter)
@@ -21,10 +21,10 @@ def parse(raw_file, delimiter):
     return parsed_data
 
 def main():
-
+    """Main"""
     new_data = parse(MY_FILE, ",")
 
-    print(new_data)
+    print new_data
 
 if __name__ == "__main__":
     main()
