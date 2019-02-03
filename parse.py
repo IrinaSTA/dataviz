3b7618f538034310db897f2cec4512396a1a1591
Irina Baldwin <2007inb@gmail.com>
2019-02-03 07:24:35 +0000

    Can parse csv into json-like object

---
+++ parse.py
@@ -0,0 +1,30 @@
+import csv
+
+MY_FILE = "sample_sfpd_incident_all.csv"
+
+def parse(raw_file, delimiter):
+    """Parses a raw CSV file to a JSON-like object"""
+    
+    opened_file = open(raw_file)
+
+    csv_data = csv.reader(opened_file, delimiter=delimiter)
+
+    parsed_data = []
+
+    fields = csv_data.__next__()
+
+    for row in csv_data:
+        parsed_data.append(dict(zip(fields, row)))
+
+    opened_file.close()
+
+    return parsed_data
+
+def main():
+
+    new_data = parse(MY_FILE, ",")
+
+    print(new_data)
+
+if __name__ == "__main__":
+    main()
